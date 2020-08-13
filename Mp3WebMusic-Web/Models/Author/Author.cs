c53fd7cfa09fe1e4d0d923ce29611a35785adb18
tin <tin11011997@gmail.com>
2020-08-13 10:17:26 +0700

    tin author typr

---
+++ Mp3WebMusic-Web/Models/Author/Author.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mp3WebMusic_Web.Models.Author
+{
+    public class Author
+    {
+        
+
+        public int AuthorID { get; set; }
+        public string AuthorName { get; set; }
+        public string Avatar { get; set; }
+        public string Introduce { get; set; }
+        public float Views { get; set; }
+        public bool IsDelete { get; set; }
+        public string Message { get; set; }
+    }
+}
